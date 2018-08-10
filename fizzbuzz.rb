8058df42f2f89552a1e401ad17116691858a320a
Byambaa Bayarmandakh <byambaa0325@gmail.com>
2018-08-10 05:31:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,13 @@
 def fizzbuzz(n)
   
   
-  if n%3==0 and n%5==0:
+  if n%3==0 and n%5==0
     return "FizzBuzz"
-  elsif n%5==0:
+  elsif n%5==0
     return "Buzz"
-  elsif n%3==0:
+  elsif n%3==0
     return "Fizz"
-  else:
+  else
     return nil
   end
 end
